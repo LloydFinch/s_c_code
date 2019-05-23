2a6bbe8be7512eae92ba906543c64e8fb0a8801d
王文奇 <wangwenqi@orangelab.cn>
2019-05-23 17:31:00 +0800

    fuck add a function

--- main.c
+++ main.c
@@ -3,6 +3,8 @@
 int sPrint(); //函数声明
 int cType();
 
+int cHex();
+
 int computeFei(int f); //函数声明的参数不需要和函数定义相同(但是建议写成相同的)
 
 //======//
@@ -14,6 +16,8 @@ int main() {
 //    computeFei(4);
 
     cType();
+
+//    cHex();
     return 0;
 }
 
@@ -47,16 +51,42 @@ int cType() {
     //sizeof
     //scanf()
 
-    float weight;
-    float value;
-    printf("Please input a float number:");
-    scanf("%f", &weight); //&weight表示找到weight变量的地点
+//    float weight;
+//    float value;
+//    printf("Please input a float number:");
+//    scanf("%f", &weight); //&weight表示找到weight变量的地点
+//
+//    value = 10 * weight;
+//    printf("value is : %.2f\n", value); //精确骚小数点后两位
 
-    value = 10 * weight;
-    printf("value is : %.2f\n", value); //精确骚小数点后两位
+//    printf("%f and %f is %f", value); //遗漏两个参数，但是不会报错，只会匹配第一个，后面没有匹配的完全是随机取数
 
-    printf("%f and %f is %f", value); //遗漏两个参数，但是不会报错，只会匹配第一个，后面没有匹配的完全是随机取数
+    int a1; //-32768 ~ 32767
+    unsigned int a; //0 ~ 65535 这个符号位现在用来表示数字了，表示的整数范围大了，没有负数范围了
 
+    char cha = 'A';
+    printf("%c\n", cha);
+    cha = 97;
+    printf("%c\n", cha);
+
+
+    return 0;
+}
+
+/**
+ * 测试进制类型
+ */
+int cHex() {
+    int a = 16;
+    printf("%d 的十进制是 %d，完整写法是%d \n", a, a, a);
+    printf("%d 的八进制是 %o，完整写法是%#o \n", a, a, a);
+    printf("%d 的十六进制是 %x，完整写法是%#x \n", a, a, a);
+}
+
+/**
+ * 测试函数
+ */
+int test() {
 
     return 0;
 }
